e3712ad57b4ea0fe22b49741c2e9a6509e2567de
Georgia Adams <georgia.adams92@gmail.com>
2021-03-05 15:31:57 +0100

    provider router updated

--- models/provider.js
+++ models/provider.js
@@ -1,19 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const providerSchema = new Schema (
-    {
-        companyName: {type: String, required: true},
-          email: {type: String, required: true, unique: true},
-          password: {type: String, required: true},
-        phoneNumber: {type: Number, required: true},
-        address: {type: String, required: true},
-          offers: [{type: Schema.Types.ObjectId, ref:'Offer'}]
-      }
-      
-);
+const providerSchema = new Schema({
+  companyName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  phoneNumber: { type: Number, required: true },
+  address: { type: String, required: true },
+  offers: [{ type: Schema.Types.ObjectId, ref: "Offer" }],
+});
 
-const Provider = mongoose.model('Provider', providerSchema);
+const Provider = mongoose.model("Provider", providerSchema);
 
 module.exports = Provider;
-
